7866c21f93500880c5220f517daaa1bbe03326d9
kirito205 <kirito205@kirito205>
2016-06-01 23:26:28 -0700

    Los programas los ise con puro radio buton sin boton porque asi es como entendi que funcionaban

---
+++ src/trabajo/Trabajo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajo;
+
+import static java.lang.System.out;
+
+/**
+ *
+ * @author kirito205
+ */
+public class Trabajo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Menu n=new Menu();
+       n.show(true);
+    }
+    
+}
